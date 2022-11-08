9b52d98322ce06d955c5b5ac9c4a7f73217180d8
mobisa-012 <mobisakwamboka@gmail.com>
2022-11-08 08:16:14 +0300

    colors file

---
+++ lib/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const homeBackground = Color(0xffffffff);
+  static const textColor = Color(0xff0174CF);
+  static const locationandCallIconColor = Color(0xffFD1F00);
+  static const searchTextColor = Color(0xff9D9D9D);
+  static const onboardingScreenColor = Color(0xffF2F8FD);
+}
