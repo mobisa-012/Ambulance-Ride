73daf3fc7bf31c87d4b9ce6ff13666edee2fd341
mobisa-012 <mobisakwamboka@gmail.com>
2022-11-09 16:24:52 +0300

    path, text and colo consts

---
+++ lib/const/path_constants.dart
@@ -0,0 +1,11 @@
+class PathConstants {
+  //icons
+  static const String call = 'assets/icons/call.png';
+  static const String help = 'assets/icons/help.png';
+  static const String home = 'assets/icons/home.png';
+  static const String hospital = 'assets/icons/hospital.png';
+  static const String payment = 'assets/icons/payment.png';
+  //images
+  static const String ambulance = 'assets/images/ambulance.png';
+  static const String profile2 = 'assets/images/profile2.png';
+}
---
+++ lib/const/text_constants.dart
@@ -0,0 +1,28 @@
+class TextConstants {
+  static const String hosy = 'HOSY';
+  static const String onboardingTitle =
+      'Have an emergency? The hospital is a tap away.';
+  static const String requestAmbulanceButton = 'Call an Ambulance';
+  static const String destination = 'Destination';
+  static const String yourLocation = 'Your location';
+  static const String distance = 'Distance';
+  static const String eta = 'ETA';
+  static const String confirmHospital = 'Confirm Hospital';
+  static const String requestAmbulance = 'Request Ambulance';
+  static const String comfortMessage =
+      'Hang in there. Your ambulance is almost here';
+  static const String hopeYouareSet =
+      'Hope you are all set! Your ambulance is here';
+  static const String stayCalm = 'Stay calm while we get you to hospital';
+  static const String weAreHere =
+      'We are here! Hold on while the doctors come to get you out';
+  static const String cancelRequest = 'Cancel Ambulance';
+  static const String ambulanceArrived = 'Your ambulance has arrived';
+  static const String drivingToHospital = 'Driving to Hospital';
+  static const String arrivedAtHospital = 'Arrived at hospital';
+  static const String choosePaymentMethod = 'Choose Payment Method';
+  static const String cash = 'Cash';
+  static const String card = 'Card';
+  static const String bankTransfer = 'Bank Transfer';
+  static const String mpesa = 'M-pesa';
+}
